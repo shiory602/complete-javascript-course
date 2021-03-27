b3500894ae07cf7baa93c9d8a497985aba03ba0c
github shiory602 <suzushioufo@gmail.com>
2021-03-27 10:54:01 -0700

    soleved

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,23 +8,17 @@
 // Hint: Use an if/else statement 😉
 // GOOD LUCK 😀
 
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
 
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
-
-// const massMark = 95;
-// const heightMark = 1.88;
-// const massJohn = 85;
-// const heightJohn = 1.76;
-
-const BMIMark = massMark / heightMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
+const BMIMark = (massMark / heightMark ** 2).toFixed(2);
+const BMIJohn = (massJohn / (heightJohn * heightJohn)).toFixed(2);
+const markHigherBMI = BMIMark > BMIJohn;
 
 if (BMIMark > BMIJohn) {
-  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+	console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
 } else {
-  console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`)
+	console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
 }
