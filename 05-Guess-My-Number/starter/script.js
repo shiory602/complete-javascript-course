0c3c28196b4781a76489c7e64bb2dbfca4a26b49
github shiory602 <suzushioufo@gmail.com>
2021-03-28 12:12:00 -0700

    Refactoring msg code

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -36,21 +36,10 @@ document.querySelector(".check").addEventListener("click", function () {
 			document.querySelector(".highscore").textContent = highScore;
 		}
 
-		// When guess is too high
-	} else if (guess > secretNumber) {
+		// When guess is wrong
+	} else if (guess !== secretNumber) {
 		if (score > 1) {
-			document.querySelector(".message").textContent = "Too high!";
-			score--;
-			document.querySelector(".score").textContent = score;
-		} else {
-			document.querySelector(".message").textContent = "You lost the game!";
-			document.querySelector(".score").textContent = 0;
-		}
-
-		// When guess is to low
-	} else if (guess < secretNumber) {
-		if (score > 1) {
-			document.querySelector(".message").textContent = "Too low!";
+			document.querySelector(".message").textContent = guess > secretNumber ? "Too high!" : "Too low!";
 			score--;
 			document.querySelector(".score").textContent = score;
 		} else {
@@ -58,6 +47,29 @@ document.querySelector(".check").addEventListener("click", function () {
 			document.querySelector(".score").textContent = 0;
 		}
 	}
+
+	// // When guess is too high
+	// else if (guess > secretNumber) {
+	// 	if (score > 1) {
+	// 		document.querySelector(".message").textContent = "Too high!";
+	// 		score--;
+	// 		document.querySelector(".score").textContent = score;
+	// 	} else {
+	// 		document.querySelector(".message").textContent = "You lost the game!";
+	// 		document.querySelector(".score").textContent = 0;
+	// 	}
+
+	// 	// When guess is to low
+	// } else if (guess < secretNumber) {
+	// 	if (score > 1) {
+	// 		document.querySelector(".message").textContent = "Too low!";
+	// 		score--;
+	// 		document.querySelector(".score").textContent = score;
+	// 	} else {
+	// 		document.querySelector(".message").textContent = "You lost the game!";
+	// 		document.querySelector(".score").textContent = 0;
+	// 	}
+	// }
 });
 
 /* Coding Challenge #1
