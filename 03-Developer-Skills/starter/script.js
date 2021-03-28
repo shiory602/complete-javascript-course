ac133085c5d30fd005fdbc9abd3e1f5967f9ed93
github shiory602 <suzushioufo@gmail.com>
2021-03-28 10:32:01 -0700

    03-01 coding challenge

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,2 +1,24 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+
+// Coding Challenge #1
+// Given an array of forecasted maximum temperatures, the thermometer displays a string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+// Your tasks:
+// 1. Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console. Try it with both test datasets.
+// 2. Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+// Test data:
+// § Data 1: [17, 21, 23]
+// § Data 2: [12, 5, -5, 0, 4]
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+let printForecast = (data) => {
+	let string = "";
+	for (let i = 0; i < data.length; i++) {
+		string += `... ${data[i]} ºC in ${i + 1} days`;
+	}
+	console.log(string + "...");
+}
+printForecast(data1);
+printForecast(data2);
