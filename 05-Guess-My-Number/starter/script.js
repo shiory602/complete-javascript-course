1ff84659a39e1298440187d41b99045ff46aab35
github shiory602 <suzushioufo@gmail.com>
2021-03-28 12:01:45 -0700

    built an app Guess My Number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,6 +11,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 
 document.querySelector(".check").addEventListener("click", function () {
@@ -29,6 +30,11 @@ document.querySelector(".check").addEventListener("click", function () {
 		document.querySelector("body").style.backgroundColor = "#60b347";
 
 		document.querySelector(".number").style.width = "30rem";
+
+		if (score > highScore) {
+			highScore = score;
+			document.querySelector(".highscore").textContent = highScore;
+		}
 
 		// When guess is too high
 	} else if (guess > secretNumber) {
@@ -77,6 +83,4 @@ document.querySelector(".again").addEventListener("click", function () {
 
 	document.querySelector("body").style.backgroundColor = "#222";
 	document.querySelector(".number").style.width = "15rem";
-
-
 })
