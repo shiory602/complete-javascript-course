b11e416a632d1b717d75d09bf3afc2084f0087fd
github shiory602 <suzushioufo@gmail.com>
2021-04-02 16:55:02 -0700

    Maps: fundamental

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -56,6 +56,40 @@ const restaurant = {
 };
 
 
+// Maps: fundamental ------------------------------------
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get('open'));
+
+const time = 23;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+const array = [1, 2];
+rest.set(array, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(array));
+
+
+
+
 
 /*
 // Sets ------------------------------------------
