858b01220ef86643d528a73b49e3729c4af31ca1
github shiory602 <suzushioufo@gmail.com>
2021-04-03 11:42:32 -0700

    String Methods Practice

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -56,7 +56,20 @@ const restaurant = {
 };
 
 
-/*- ---------------------------------
+// String Methods Practice
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🎈' : ''} ${type.replaceAll('_', ' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+  console.log(output);
+}
+
+
+//- ---------------------------------
 /* Coding Challenge #4
 Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
 The input will come from a textarea inserted into the DOM (see code below to insert the elements), and conversion will happen when the button is pressed.
@@ -81,6 +94,7 @@ name conversion working �
 you're stuck. Then pause and continue!
 Afterwards, test with your own test data! */
 
+/*
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
@@ -93,8 +107,8 @@ document.querySelector('button').addEventListener('click', function () {
       const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
       console.log(`${output.padEnd(20, ' ')}${'🌸'.repeat(i + 1)}`);
     }
-  }) *
-  /
+  });
+*/
 
 /*
 ///////////////////////////////////////////////
