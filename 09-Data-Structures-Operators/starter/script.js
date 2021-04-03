2bdde435630e60edbc125e67abcf3f7c71f83ec1
github shiory602 <suzushioufo@gmail.com>
2021-04-02 17:17:33 -0700

    Maps: iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -55,9 +55,42 @@ const restaurant = {
   }
 };
 
+/*
+// Maps: iteration ------------------------------
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
 
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+/////////////////////////////////////////////////
+*/
+
+/*
 // Maps: fundamental ------------------------------------
-
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
 rest.set(1, 'Firenze, Italy');
@@ -86,7 +119,8 @@ console.log(rest);
 console.log(rest.size);
 
 console.log(rest.get(array));
-
+////////////////////////////////////////////////
+*/
 
 
 
