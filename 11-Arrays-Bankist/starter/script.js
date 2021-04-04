2a8704f7ba1eb8fa0baf8af04511e6901edde435
github shiory602 <suzushioufo@gmail.com>
2021-04-03 23:27:42 -0700

    simple array method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,35 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE arrは変わらない
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE  mutable(変わる)
+// console.log(arr.splice(2));
+arr.splice(-1);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE mutable（変わる）
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT not mutable(変わらない)
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log(arr);
+console.log([...arr, ...arr2]);
+
+// JOIN mutable(変わる)
+console.log(letters.join(' - '));
