44e8de7873588f2b826fbd3c1db2aa3efc612eb9
github shiory602 <suzushioufo@gmail.com>
2021-04-04 01:53:57 -0700

    Looping Arrays: forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,10 +71,13 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+/*
+///////////////////////////////////////////////////
+// Array method
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // SLICE arrは変わらない
@@ -106,3 +109,31 @@ console.log([...arr, ...arr2]);
 
 // JOIN mutable(変わる)
 console.log(letters.join(' - '));
+*/
+
+
+////////////////////////////////////////////////////////
+//
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i+1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('--- FOREACH ---');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+// 0: function(200)
+// 0: function(450)
+// 0: function(400)
+// ...
