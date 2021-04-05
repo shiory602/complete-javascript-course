813d8502a2cb49fed64bacf571cc21295fa7fb9e
github shiory602 <suzushioufo@gmail.com>
2021-04-04 22:36:17 -0700

    function return function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -58,7 +58,7 @@ newPassport(jonas);
 checkIn(flight, jonas);
 */
 
-
+/*
 const oneWord = function (str) {
 	return str.replace(/ /g, '').toLowerCase();
 }
@@ -85,3 +85,21 @@ const high5 = function () {
 };
 document.body.addEventListener('click', high5);
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
+*/
+
+// Function returns function
+const greet = function (greeting) {
+	return function (name) {
+		console.log(`${greeting} ${name}`);
+	};
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+// Challenge
+const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
+greetArrow('Hi')('Jonas');
