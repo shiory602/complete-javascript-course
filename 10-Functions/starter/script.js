5aac78e9f0cdc72e7b2852a85d78e9014a81adc2
github shiory602 <suzushioufo@gmail.com>
2021-04-04 22:30:54 -0700

    callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -23,6 +23,8 @@ createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
 */ ///////
 
+
+/*
 const flight = 'LH234';
 const jonas = {
 	name: 'Jonas Schmedmann',
@@ -54,3 +56,32 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+
+const oneWord = function (str) {
+	return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWorld = function (str) {
+	const [first, ...others] = str.split(' ');
+	return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order functions
+const transformer = function (str, fn) {
+	console.log(`Original string: ${str}`);
+	console.log(`Transformed string: ${fn(str)}`);
+	console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('JavaScript is the best!', upperFirstWorld);
+
+transformer('Javasctipt is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+	console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
