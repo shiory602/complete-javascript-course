22e9fff7e748c01cf33ce74a0611aa870eb0e8d0
github shiory602 <suzushioufo@gmail.com>
2021-04-14 13:06:43 -0700

    prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -23,4 +23,27 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person); // true
-console.log(john instanceof Person); // false
+// console.log(john instanceof Person); // false
+
+// Prototypes
+Person.prototype.calcAge = function() {
+    console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOf.likedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
