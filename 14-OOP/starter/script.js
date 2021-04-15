23d17b9288cace5d61ba6e2787fe18da83cf99fe
github shiory602 <suzushioufo@gmail.com>
2021-04-14 20:40:07 -0700

    ES6 Classes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -125,6 +125,58 @@ car2.accelerate();
 
 
 
+
+/*
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+    constructor(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+    
+    // Methods will be added to .prototype property
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    };
+
+    greet () {
+        console.log(`Hey ${this.firstName}`);
+    };
+}
+    
+    const jessica = new PersonCl('Jessica', 1996);
+    console.log(jessica);
+    jessica.calcAge();
+    
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//     console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizes
+// 3. Classes are executed in strict mode
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //215. Inheritance Between "Classes": Constructor Functions
 
 const Student = function(firstName, birthYear, course) {
