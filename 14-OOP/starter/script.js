aa6777210820d5020177824509ec952c0dd07168
github shiory602 <suzushioufo@gmail.com>
2021-04-15 13:27:47 -0700

    encapusulation

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -471,6 +471,82 @@ jay.introduce();
 jay.calcAge();
 */
 
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+
 class Account {
-    constructor(owner, currency, pin)
+    // 1) Public fields (instances)
+    locale = navigator.language;
+    _movements = [];
+
+    // 2) Private fields (instances)
+    #movements = [];
+    #pin;
+
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        // Protected property
+        this.#pin = pin;
+        // this._movements = [];
+        // this.locale = navigator.language;
+
+        console.log(`Thanks for opening an Account, ${owner}`);
+    }
+
+    // Public interface
+    getMovements() {
+        return this.#movements;
+    }
+
+    deposit(val) {
+        this.#movements.push(val);
+    }
+
+    withdraw(val) {
+        this.deposit(-val)
+    }
+
+    _approveLoan(val) {
+        return true;
+    }
+
+    requestLoan(val) {
+        // if(this.#approveLoan(val)) {
+        if(this._approveLoan(val)) {
+            this.deposit(val);
+            console.log(`Loan approved`);
+        }
+    }
+
+    static helper() {
+        console.log('Helper');
+    }
+
+    // 4) Private methods
+    // #approveLoan(val) {
+    _approveLoan(val) {
+        return true;
+    }
+
+
 }
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+// acc1._approveLoan(1000);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+Account.helper();
