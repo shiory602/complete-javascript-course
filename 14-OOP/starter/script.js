3fe44d66a97ab2e390d29d3a304c98c9bed12908
github shiory602 <suzushioufo@gmail.com>
2021-04-14 20:30:44 -0700

    Prototypal Inheritance and The Prototype Cahin challenge1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const Person = function(firstName, birthYear) {
     this.firstName = firstName;
     this.birthYear = birthYear;
@@ -9,6 +9,7 @@ const Person = function(firstName, birthYear) {
     //     console.log(2037 - this.birthYear);
     // };
 };
+
 
 const jonas = new Person('Jonas', 1991);
 console.log(jonas);
@@ -25,11 +26,13 @@ console.log(matilda, jack);
 console.log(jonas instanceof Person); // true
 // console.log(john instanceof Person); // false
 
+
 // Prototypes
 Person.prototype.calcAge = function() {
     console.log(2037 - this.birthYear);
 };
 
+/*
 jonas.calcAge();
 matilda.calcAge();
 
@@ -39,11 +42,100 @@ console.log(jonas.__proto__ === Person.prototype);
 console.log(Person.prototype.isPrototypeOf(jonas));
 console.log(Person.prototype.isPrototypeOf(matilda));
 console.log(Person.prototype.isPrototypeOf(Person));
+*/
 
 // .prototypeOf.likedObjects
 
-Person.prototype.species = 'Homo Sapiens';
-console.log(jonas.species, matilda.species);
 
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(jonas.species, matilda.species);
+
+// console.log(jonas.hasOwnProperty('firstName'));
+// console.log(jonas.hasOwnProperty('species'));
+
+
+
+
+
+
+
+
+/*
+//Object.prototype (top of prototype chain)
+console.log(jonas.__proto__);
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__); // null
+
+console.dir(Person.prototype.constructor);
+
+const arr = [2, 6, 4, 5, 6, 9, 3]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function() {
+    return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+*/
+///////////////////////////////////////
+/*
+Coding Challenge #1
+Your tasks:
+1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 'speed' property. The 'speed' property is the current speed of the car in km/h
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console
+4. Create 2 'Car' objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them
+Test data:
+§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 2: 'Mercedes' going at 95 km/h
+GOOD LUCK 😀
+*/
+/*
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;　// km/h
+};
+
+Car.prototype.accelerate = function() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function() {
+    this.speed -= 5
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+}
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+car1.accelerate();
+car2.accelerate();
+*/
+
+
+
+
+//215. Inheritance Between "Classes": Constructor Functions
+
+const Student = function(firstName, birthYear, course) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+    this.course = course;
+}
+
+Student.prototype.introduce = function() {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+}
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+mike.introduce();
