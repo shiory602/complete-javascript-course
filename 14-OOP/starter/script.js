5e840cdcb5996b68e6e6cd861fe1126fa7e4c323
github shiory602 <suzushioufo@gmail.com>
2021-04-15 14:16:34 -0700

    challenge 4

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -475,7 +475,7 @@ jay.calcAge();
 // 2) Private fields
 // 3) Public methods
 // 4) Private methods
-
+/*
 class Account {
     // 1) Public fields (instances)
     locale = navigator.language;
@@ -554,3 +554,79 @@ Account.helper();
 // Chaining
 acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1.getMovements());
+*/
+
+/////////////////////////////////////////////////////////////////////////
+/*
+Coding Challenge #4
+Your tasks:
+1. Re-create Challenge#3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. Then experiment with chaining!
+Test data:
+§ Data car 1: 'Rivian' going at 120 km/h, with a charge of 23% GOOD LUCK 😀
+*/
+
+class CarCl {
+    constructor(model, speed) {
+        this.model = model;
+        this.speed = speed; // km/h
+    }
+
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.model} is going at ${this.speed} km/h`);
+    }
+
+    brake() {
+        this.speed -= 5
+        console.log(`${this.model} is going at ${this.speed} km/h`);
+        return this;
+    }
+
+    get speedUS() {
+        return this.speed / 1.6;
+    }
+
+    set speedUS(speed) {
+        this.speed = speed * 1.6;
+    }
+}
+
+
+class EVCl extends CarCl {
+    #charge;
+
+    constructor(model, speed, charge) {
+        super(model, speed);
+        this.#charge = charge;
+    }
+
+    chargeBattery(chargeTo) {
+        this.#charge = chargeTo;
+        return this;
+    }
+
+    accelerate() {
+        this.speed += 20;
+        this.#charge--;
+        console.log(
+            `${this.model} going at ${this.speed} km/h, with a charge of ${this.#charge}%`
+        );
+        return this;
+    }
+}
+
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+// console.log(rivian.#charge);
+rivian
+    .accelerate()
+    .accelerate()
+    .accelerate()
+    .brake()
+    .chargeBattery(50)
+    .accelerate();
+
+console.log(rivian.speedUS);
